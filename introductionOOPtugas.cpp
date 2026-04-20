2d0b211dbd8dbe3dbbd3055bb4761a7b56c47b69
akmalropip <akmalropip@gmail.com>
2026-04-20 14:44:08 +0700

    menambahkan void

--- introductionOOPtugas.cpp
+++ introductionOOPtugas.cpp
@@ -8,3 +8,10 @@ class Barang{
     string kategori;
     string tanggalProduksi;
 
+ void printData(){
+        cout << "Nama : " << nama << endl;
+        cout << "Jumlah : " << jumlah << endl;
+        cout << "Kategori : " << kategori << endl;
+        cout << "TanggalProduksi : " << tanggalProduksi << endl;
+    }
+}; //batas class
