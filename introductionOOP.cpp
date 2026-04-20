7288262424303067eb71b69ac83ab2575dd9a5ed
akmalropip <akmalropip@gmail.com>
2026-04-20 14:27:05 +0700

    menambahkan void

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -7,5 +7,10 @@ class Mahasiswa{
     string nama;
     float nilai;
 
-    
+    void printData(){
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
+}; //batas class
 
